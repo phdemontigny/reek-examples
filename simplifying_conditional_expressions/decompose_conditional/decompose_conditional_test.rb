e566c2e028a728f0d83b8a334d6ea9263aefce69
Philippe Demontigny <phdemontigny@gmail.com>
2017-02-12 14:50:38 +0000

    Adds decompose conditional example, rearranges code by refactoring technique.

---
+++ simplifying_conditional_expressions/decompose_conditional/decompose_conditional_test.rb
@@ -0,0 +1,28 @@
+require 'test/unit'
+require_relative 'decompose_conditional'
+
+class DecomposeConditionalTest < Test::Unit::TestCase
+  include DecomposeConditional
+
+  def test_raindrops
+    assert_equal("Pling", raindrops(12))
+    assert_equal("Plang", raindrops(20))
+    assert_equal("Plong", raindrops(28))
+    assert_equal("PlingPlang", raindrops(15))
+    assert_equal("PlingPlong", raindrops(21))
+    assert_equal("PlangPlong", raindrops(35))
+    assert_equal("PlingPlangPlong", raindrops(105))
+    assert_equal("34", raindrops(34))
+  end
+
+  def test_raindrops_fixed
+    assert_equal("Pling", raindrops_fixed(12))
+    assert_equal("Plang", raindrops_fixed(20))
+    assert_equal("Plong", raindrops_fixed(28))
+    assert_equal("PlingPlang", raindrops_fixed(15))
+    assert_equal("PlingPlong", raindrops_fixed(21))
+    assert_equal("PlangPlong", raindrops_fixed(35))
+    assert_equal("PlingPlangPlong", raindrops_fixed(105))
+    assert_equal("34", raindrops_fixed(34))
+  end
+end
